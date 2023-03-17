51f09dcd96bd5bbdf990cbcc6c19f5a6527add98
RameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2023-03-17 17:18:42 +0530

    added add functionality partially

--- script.js
+++ script.js
@@ -1,86 +1,86 @@
-const chemicalSuppliesArray = [{
-        id: 1,
-        chemicalName: "Ammonium Persulfate",
-        vendor: "LG Chem",
-        density: 3525.92,
-        viscosity: 60.63,
-        packaging: "Bag",
-        packSize: 100.00,
-        unit: "kg",
-        quantity: 6495.18
-    },
-    {
-        id: 2,
-        chemicalName: "Caustic Potash",
-        vendor: "Formosa",
-        density: 3172.15,
-        viscosity: 48.22,
-        packaging: "Bag",
-        packSize: 100.00,
-        unit: "kg",
-        quantity: 8751.90
-    },
-    {
-        id: 3,
-        chemicalName: "Dimethylaminopropylamion",
-        vendor: "LG Chem",
-        density: 8435.37,
-        viscosity: 12.62,
-        packaging: "Barrel",
-        packSize: 75.00,
-        unit: "L",
-        quantity: 5964.61
-    },
-    {
-        id: 4,
-        chemicalName: "Mono Ammonium Phosphate",
-        vendor: "LG Chem",
-        density: 3525.92,
-        viscosity: 60.63,
-        packaging: "bag",
-        packSize: 100.00,
-        unit: "kg",
-        quantity: 6495.18
-    },
-    {
-        id: 5,
-        chemicalName: "Ferric Nitrate",
-        vendor: "DowDuPont",
-        density: 364.04,
-        viscosity: 14.90,
-        packaging: "Bag",
-        packSize: 105.00,
-        unit: "kg",
-        quantity: 4154.33
-    },
-    {
-        id: 6,
-        chemicalName: "n-Pentane",
-        vendor: "Sinopec",
-        density: 4535.26,
-        viscosity: 66.76,
-        packaging: "N/A",
-        packSize: "N/A",
-        unit: "t",
-        quantity: 6272.34
-    },
-    {
-        id: 7,
-        chemicalName: "Glycol Ether PM",
-        vendor: "LG Chem",
-        density: 6495.18,
-        viscosity: 72.12,
-        packaging: "Bag",
-        packSize: 250.00,
-        unit: "Kg",
-        quantity: 8749.54
-    }
-
+const chemicalSuppliesArray = [
+  {
+    id: 1,
+    chemicalName: "Ammonium Persulfate",
+    vendor: "LG Chem",
+    density: 3525.92,
+    viscosity: 60.63,
+    packaging: "Bag",
+    packSize: 100.0,
+    unit: "kg",
+    quantity: 6495.18,
+  },
+  {
+    id: 2,
+    chemicalName: "Caustic Potash",
+    vendor: "Formosa",
+    density: 3172.15,
+    viscosity: 48.22,
+    packaging: "Bag",
+    packSize: 100.0,
+    unit: "kg",
+    quantity: 8751.9,
+  },
+  {
+    id: 3,
+    chemicalName: "Dimethylaminopropylamion",
+    vendor: "LG Chem",
+    density: 8435.37,
+    viscosity: 12.62,
+    packaging: "Barrel",
+    packSize: 75.0,
+    unit: "L",
+    quantity: 5964.61,
+  },
+  {
+    id: 4,
+    chemicalName: "Mono Ammonium Phosphate",
+    vendor: "LG Chem",
+    density: 3525.92,
+    viscosity: 60.63,
+    packaging: "bag",
+    packSize: 100.0,
+    unit: "kg",
+    quantity: 6495.18,
+  },
+  {
+    id: 5,
+    chemicalName: "Ferric Nitrate",
+    vendor: "DowDuPont",
+    density: 364.04,
+    viscosity: 14.9,
+    packaging: "Bag",
+    packSize: 105.0,
+    unit: "kg",
+    quantity: 4154.33,
+  },
+  {
+    id: 6,
+    chemicalName: "n-Pentane",
+    vendor: "Sinopec",
+    density: 4535.26,
+    viscosity: 66.76,
+    packaging: "N/A",
+    packSize: "N/A",
+    unit: "t",
+    quantity: 6272.34,
+  },
+  {
+    id: 7,
+    chemicalName: "Glycol Ether PM",
+    vendor: "LG Chem",
+    density: 6495.18,
+    viscosity: 72.12,
+    packaging: "Bag",
+    packSize: 250.0,
+    unit: "Kg",
+    quantity: 8749.54,
+  },
 ];
 
-localStorage.setItem("iitbProject", JSON.stringify(chemicalSuppliesArray))
+localStorage.setItem("iitbProject", JSON.stringify(chemicalSuppliesArray));
 
-const chemicalSupplies = JSON.parse(localStorage.getItem("iitbProject"))
+const chemicalSupplies = JSON.parse(localStorage.getItem("iitbProject"));
 const addButton = document.getElementById("add");
 const editButton = document.getElementById("edit");
 const moveButton = document.getElementById("move-up");
@@ -88,18 +88,14 @@ const moveDownButton = document.getElementById("move-down");
 const deleteButton = document.getElementById("delete");
 const refreshButton = document.getElementById("refresh");
 const saveButton = document.getElementById("save");
-const tableBody = document.getElementById("table-body")
-    // checkbox variable
-    // addButton.addEventListener("click", add)
+const tableBody = document.getElementById("table-body");
+addButton.addEventListener("click", add);
 
 const selectall = document.getElementById("chkbox");
-console.log(chemicalSupplies)
 
 function render() {
-    chemicalSupplies.map((row) => {
-
-        return tableBody.innerHTML +=
-            `<tr class="data">
+  chemicalSupplies.map((row) => {
+    return (tableBody.innerHTML += `<tr class="data">
         <td>
             <div class="content" id="select-all">
                 <input id="chkbox"  type="checkbox" />
@@ -150,12 +146,64 @@ function render() {
             ${row.quantity}
             </div>
         </td>
-    </tr>`
-    })
+    </tr>`);
+  });
 }
 render();
 
-function add() {}
+function add() {
+  tableBody.innerHTML += `
+  <tr>
+  <td>
+            <div class="content" id="select-all">
+                <input id="chkbox"  type="checkbox" />
+            </div>
+        </td>
+        <td>
+        <div class="content">
+            ${chemicalSupplies.length + 1}
+        </div>
+    </td>
+    <td>
+        <input type="text" id = "new-chemicalName"/>
+    </td>
+    <td>
+        <input type="text" id = "new-vendor"/>
+    </td>
+    <td>
+        <input type="number" id = "new-density"/>
+    </td>
+    <td>
+        <input type="number" id = "new-viscosity"/>
+    </td>
+    <td>
+        <select  id = "new-packaging">
+        <option>Select</option>
+        <option>Bag</option>
+        <option>Bottle</option>
+        <option>Barrel</option>
+        <option>N/A</option>
+        </select>
+    </td>
+    <td>
+        <input type="number" id = "new-packSize"/>
+    </td>
+    <td>
+        <select  id = "new-unit">
+        <option>Select</option>
+        <option>kg</option>
+        <option>gm</option>
+        <option>L</option>
+        <option>ml</option>
+        <option>t</option>
+        </select>
+    </td>
+    <td>
+        <input type="number" id = "new-quantity"/>
+    </td>
+  </tr>
+  `;
+}
 
 function edit() {}
 
@@ -175,12 +223,11 @@ function sortDescending() {}
 
 function generateId() {}
 
-
-
 /*-----------------------------
 code for select all button
 ------------------------------*/
 
-selectall.addEventListener('click', function() {
-    console.log("Clicked!");
+selectall.addEventListener("click", function () {
+  console.log("Clicked!");
 });
+addButton.addEventListener("click", add);
